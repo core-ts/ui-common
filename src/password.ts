3b81450bfb30fbc9f3ae674183512e974403a46a
Duc Nguyen <minhduc140583@gmail.com>
2025-01-12 23:42:05 +0700

    Add password

---
+++ src/password.ts
@@ -0,0 +1,199 @@
+function changePassword(e: Event) {
+  e.preventDefault()
+  const resource = getResource()
+  const target = e.target as HTMLButtonElement
+  const form = target.form as HTMLFormElement
+  const eleMessage = form.querySelector(".message") as Element
+  const txtUsername = getElement(form, "username") as HTMLInputElement
+  const username = txtUsername.value
+  if (username === "") {
+    const label = getLabel(txtUsername)
+    const msg = format(resource.error_required, label)
+    showErrorMessage(eleMessage, msg)
+    return
+  }
+  const txtCurrentPassword = getElement(form, "currentPassword") as HTMLInputElement
+  const currentPassword = txtCurrentPassword.value
+  if (currentPassword === "") {
+    const label = getLabel(txtCurrentPassword)
+    const msg = format(resource.error_required, label)
+    showErrorMessage(eleMessage, msg)
+    return
+  }
+  const txtPassword = getElement(form, "password") as HTMLInputElement
+  const passwordLabel = getLabel(txtPassword)
+  const password = txtPassword.value
+  if (password === "") {
+    const msg = format(resource.error_required, passwordLabel)
+    showErrorMessage(eleMessage, msg)
+    return
+  }
+  if (!resources.password.test(password)) {
+    const msg = format(resource.error_password, passwordLabel)
+    showErrorMessage(eleMessage, msg)
+    return
+  }
+  const txtConfirmPassword = getElement(form, "confirmPassword") as HTMLInputElement
+  const confirmPassword = txtConfirmPassword.value
+  if (confirmPassword !== password) {
+    const label = getLabel(txtConfirmPassword)
+    const msg = format(resource.error_equal, label, passwordLabel)
+    showErrorMessage(eleMessage, msg)
+    return
+  }
+  const url = getCurrentURL()
+  const user = { username, currentPassword, password }
+  fetch(url, {
+    method: "POST",
+    headers: getHttpHeaders(),
+    body: JSON.stringify(user),
+  })
+    .then((response) => {
+      if (response.ok) {
+        response.json().then((result) => {
+          if (result) {
+            showInfoMessage(eleMessage, resource.success_change_password)
+          } else {
+            showErrorMessage(eleMessage, resource.fail_change_password)
+          }
+        })
+      } else {
+        if (response.status === 401) {
+          response.text().then((msg) => {
+            showErrorMessage(eleMessage, msg)
+          })
+        } else if (response.status === 409) {
+          showErrorMessage(eleMessage, resource.password_duplicate)
+        } else if (response.status === 403) {
+          showErrorMessage(eleMessage, resource.fail_change_password)
+        } else {
+          console.error("Error: ", response.statusText)
+          alertErrorWithDetails(resource.error_submit_failed, response.statusText)
+        }
+      }
+    })
+    .catch((err) => {
+      hideLoading()
+      console.log("Error: " + err)
+      alertErrorWithDetails(resource.error_submitting_form, err)
+    })
+}
+function forgotPassword(e: Event) {
+  e.preventDefault()
+  const resource = getResource()
+  const target = e.target as HTMLButtonElement
+  const form = target.form as HTMLFormElement
+  const eleMessage = form.querySelector(".message") as Element
+  const txtContact = getElement(form, "contact") as HTMLInputElement
+  const contact = txtContact.value
+  if (contact === "") {
+    const label = getLabel(txtContact)
+    const msg = format(resource.error_required, label)
+    showErrorMessage(eleMessage, msg)
+    return
+  }
+  if (!isEmail(contact) && !isUsername(contact)) {
+    showErrorMessage(eleMessage, resource.error_contact_exp)
+    return
+  }
+  const url = getCurrentURL()
+  const user = { contact }
+  fetch(url, {
+    method: "POST",
+    headers: getHttpHeaders(),
+    body: JSON.stringify(user),
+  })
+    .then((response) => {
+      if (response.ok) {
+        response.json().then((result) => {
+          showInfoMessage(eleMessage, resource.success_forgot_password)
+        })
+      } else {
+        if (response.status === 404) {
+          showErrorMessage(eleMessage, resource.fail_forgot_password)
+        } else {
+          console.error("Error: ", response.statusText)
+          alertErrorWithDetails(resource.error_submit_failed, response.statusText)
+        }
+      }
+    })
+    .catch((err) => {
+      hideLoading()
+      console.log("Error: " + err)
+      alertErrorWithDetails(resource.error_submitting_form, err)
+    })
+}
+function resetPassword(e: Event) {
+  e.preventDefault()
+  const resource = getResource()
+  const target = e.target as HTMLButtonElement
+  const form = target.form as HTMLFormElement
+  const eleMessage = form.querySelector(".message") as Element
+  const txtUsername = getElement(form, "username") as HTMLInputElement
+  const username = txtUsername.value
+  if (username === "") {
+    const label = getLabel(txtUsername)
+    const msg = format(resource.error_required, label)
+    showErrorMessage(eleMessage, msg)
+    return
+  }
+  const txtPasscode = getElement(form, "passcode") as HTMLInputElement
+  const passcode = txtPasscode.value
+  if (passcode === "") {
+    const label = getLabel(txtPasscode)
+    const msg = format(resource.error_required, label)
+    showErrorMessage(eleMessage, msg)
+    return
+  }
+  const txtPassword = getElement(form, "password") as HTMLInputElement
+  const passwordLabel = getLabel(txtPassword)
+  const password = txtPassword.value
+  if (password === "") {
+    const msg = format(resource.error_required, passwordLabel)
+    showErrorMessage(eleMessage, msg)
+    return
+  }
+  if (!resources.password.test(password)) {
+    const msg = format(resource.error_password, passwordLabel)
+    showErrorMessage(eleMessage, msg)
+    return
+  }
+  const txtConfirmPassword = getElement(form, "confirmPassword") as HTMLInputElement
+  const confirmPassword = txtConfirmPassword.value
+  if (confirmPassword !== password) {
+    const label = getLabel(txtConfirmPassword)
+    const msg = format(resource.error_equal, label, passwordLabel)
+    showErrorMessage(eleMessage, msg)
+    return
+  }
+  const url = getCurrentURL()
+  const user = { username, passcode, password }
+  fetch(url, {
+    method: "POST",
+    headers: getHttpHeaders(),
+    body: JSON.stringify(user),
+  })
+    .then((response) => {
+      if (response.ok) {
+        showInfoMessage(eleMessage, resource.success_reset_password)
+      } else {
+        if (response.status === 401) {
+          response.text().then((msg) => {
+            showErrorMessage(eleMessage, msg)
+          })
+        } else if (response.status === 409) {
+          showErrorMessage(eleMessage, resource.password_duplicate)
+        } else if (response.status === 403) {
+          showErrorMessage(eleMessage, resource.fail_reset_password)
+        } else {
+          console.error("Error: ", response.statusText)
+          alertErrorWithDetails(resource.error_submit_failed, response.statusText)
+        }
+      }
+    })
+    .catch((err) => {
+      hideLoading()
+      console.log("Error: " + err)
+      alertErrorWithDetails(resource.error_submitting_form, err)
+    })
+}
