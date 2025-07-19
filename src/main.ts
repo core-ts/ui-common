bac86a4128efd9e3aff317f8231cb6297c4326e9
Duc Nguyen <minhduc140583@gmail.com>
2025-07-19 20:05:50 +0700

    Remove main

--- src/main.ts
+++
@@ -1,6 +0,0 @@
-resources.load = function (pageBody: HTMLElement) {
-  const codeBlocks = pageBody.querySelectorAll("code")
-  for (let i = 0; i < codeBlocks.length; i++) {
-    Prism.highlightElement(codeBlocks[i])
-  }
-}
